940f820ed3e8320218ae8e5ce96a643436c910d4
Chaksawat <88503339+Nonene2546@users.noreply.github.com>
2023-10-10 16:00:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 let volume_icon_status = 1
 
+document.addEventListener('DOMContentLoaded', function(){
+  create_transition_block()
+})
+
 document.getElementById("volume-icon").addEventListener("click", function(){
   slider = document.getElementById("volume-slider")
   if(volume_icon_status){
@@ -22,6 +26,30 @@ document.getElementById("volume-icon").addEventListener("click", function(){
   volume_icon_status = !volume_icon_status
 })
 
+function create_transition_block(){
+  const container = document.getElementById('transition-container')
+  let height = 30
+  let curWidth = window.innerWidth * 1.2
+  let sumHeight = 0
+
+  while(sumHeight < window.innerHeight){
+    const randomHeight = height
+    const randomWidth = curWidth
+    curWidth += 50
+
+    // Create the animated block
+    const animatedBlock = document.createElement('div')
+    animatedBlock.id = 'animatedBlock'
+    animatedBlock.classList.add('transition-block')
+    animatedBlock.style.height = randomHeight + 'px'
+    animatedBlock.style.width = randomWidth + 'px'
+
+    // Add the block to the container
+    container.appendChild(animatedBlock);
+    sumHeight += randomHeight
+  }
+}
+
 document.getElementById("volume-slider").addEventListener("input", function(){
   icon = document.getElementById("volume-icon")
   if(this.value >= 0.5){
@@ -42,4 +70,15 @@ document.getElementById("volume-slider").addEventListener("input", function(){
   }
   this.style.opacity = 0.7
   volume_icon_status = 1
+})
+
+document.getElementById("play-button").addEventListener('click', function() {
+  const container = document.getElementById('transition-container')
+
+  container.style.animation = 'sliding-left-in 2s forwards'
+
+  setTimeout(function(){
+    container.style.animation = 'sliding-left-out 2s forwards'
+  }, 3000)
+
 })
